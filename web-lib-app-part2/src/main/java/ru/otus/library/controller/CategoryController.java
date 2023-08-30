b293e54f9bed9aefd6fc1ccc8d743b864effc9e6
Takhir Amanov <shadowelfa@yandex.ru>
2023-08-30 16:27:53 +0400

    web-lib-app-part2: fix

--- web-lib-app-part2/src/main/java/ru/otus/library/controller/CategoryController.java
+++ web-lib-app-part2/src/main/java/ru/otus/library/controller/CategoryController.java
@@ -1,7 +1,6 @@
 package ru.otus.library.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +12,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/category")
 @AllArgsConstructor
-@CrossOrigin(origins = "*")
 public class CategoryController {
 
     private final CategoryHandler handler;
