85e53114dd5eee6ca74dc881ab18ebfaf11740e9
Roman Amanov <Roman.amanov@avotech.uz>
2023-09-20 11:18:00 +0300

    security: tests

--- security-app/src/test/java/ru/otus/library/controller/BookControllerTest.java
+++ security-app/src/test/java/ru/otus/library/controller/BookControllerTest.java
@@ -1,29 +1,25 @@
 package ru.otus.library.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import ru.otus.library.appConfig.SecurityConfig;
 import ru.otus.library.dto.BookDto;
 import ru.otus.library.handlers.BookHandler;
 
-import java.util.Collections;
-
-import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -39,86 +35,48 @@ class BookControllerTest {
     @MockBean
     private BookHandler handler;
 
-    @Test
-    @DisplayName("вернуть все книги")
-    @WithMockUser
-    void shouldGetAllBooks() throws Exception {
-        when(handler.getAllBooks()).thenReturn(Collections.emptyList());
+    @ParameterizedTest
+    @CsvSource({
+            "/api/book/,GET,true",
+            "/api/book/123,GET,true",
+            "/api/book/,POST,true",
+            "/api/book/123,PUT,true",
+            "/api/book/123,DELETE,true",
 
-        mvc.perform(get("/api/book/"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$").isArray())
-                .andExpect(content().contentType(APPLICATION_JSON));
+            "/api/book/,GET,false",
+            "/api/book/123,GET,false",
+            "/api/book/,POST,false",
+            "/api/book/123,PUT,false",
+            "/api/book/123,DELETE,false"
+    })
+    void shouldAccessEndpoint(String endpoint, String method, boolean withUser) throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = getRequestBuilder(endpoint, method);
+
+        if (withUser) {
+            mvc.perform(requestBuilder.with(user("user").password("password")))
+                    .andDo(print())
+                    .andExpect(status().isOk());
+        } else {
+            mvc.perform(requestBuilder)
+                    .andDo(print())
+                    .andExpect(status().is3xxRedirection());
+        }
     }
 
-    @Test
-    @DisplayName("перенаправить запрос на /login")
-    void shouldRedirectRequestToLogin() throws Exception {
-        when(handler.getAllBooks()).thenReturn(Collections.emptyList());
-
-        mvc.perform(get("/api/book/"))
-                .andDo(print())
-                .andExpect(status().is3xxRedirection());
-    }
-
-    @Test
-    @DisplayName("удалить книгу")
-    @WithMockUser
-    void shouldDeleteBook() throws Exception {
-        mvc.perform(delete("/api/book/123"))
-                .andDo(print())
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    @DisplayName("обновить книгу")
-    @WithMockUser
-    void shouldUpdateBook() throws Exception {
+    private static MockHttpServletRequestBuilder getRequestBuilder(String endpoint, String method) throws JsonProcessingException {
         BookDto dto = BookDto.builder().id("123").title("Sample Book").build();
-
-        when(handler.updateBook(dto)).thenReturn(dto);
-
-        mvc.perform(MockMvcRequestBuilders.put("/api/book/123")
-                        .accept(APPLICATION_JSON)
-                        .contentType(APPLICATION_JSON)
-                        .content(new ObjectMapper().writeValueAsString(dto)))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.title").value("Sample Book"))
-                .andExpect(content().contentType(APPLICATION_JSON));
-    }
-
-    @Test
-    @DisplayName("добавить книгу")
-    @WithMockUser
-    void shouldAddPage() throws Exception {
-        BookDto dto = BookDto.builder().id("123").title("Sample Book").build();
-
-        when(handler.addBook(dto)).thenReturn(dto);
-
-        mvc.perform(MockMvcRequestBuilders.post("/api/book/")
-                        .accept(APPLICATION_JSON)
-                        .contentType(APPLICATION_JSON)
-                        .content(new ObjectMapper().writeValueAsString(dto)))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.title").value("Sample Book"))
-                .andExpect(content().contentType(APPLICATION_JSON));
-    }
-
-    @Test
-    @DisplayName("показать содержимое книги")
-    @WithMockUser
-    void viewPage() throws Exception {
-        BookDto dto = BookDto.builder().id("123").title("Sample Book").build();
-
-        when(handler.getBook(dto.getId())).thenReturn(dto);
-
-        mvc.perform(MockMvcRequestBuilders.get("/api/book/123"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.title").value("Sample Book"))
-                .andExpect(content().contentType(APPLICATION_JSON));
+        return switch (method) {
+            case "GET" -> MockMvcRequestBuilders.get(endpoint);
+            case "POST" -> MockMvcRequestBuilders.post(endpoint)
+                    .accept(APPLICATION_JSON)
+                    .contentType(APPLICATION_JSON)
+                    .content(new ObjectMapper().writeValueAsString(dto));
+            case "PUT" -> MockMvcRequestBuilders.put(endpoint)
+                    .accept(APPLICATION_JSON)
+                    .contentType(APPLICATION_JSON)
+                    .content(new ObjectMapper().writeValueAsString(dto));
+            case "DELETE" -> MockMvcRequestBuilders.delete(endpoint);
+            default -> throw new IllegalArgumentException("Unsupported HTTP method: " + method);
+        };
     }
 }
